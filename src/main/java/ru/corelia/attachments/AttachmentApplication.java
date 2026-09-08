c53ff19ff98cf8f0278de6c272d8e2563505e058
ilyabaykalov <ibaykalov@blogic.ru>
2026-09-08 17:35:15 +0300

    Создан каркас сервиса corelia-attachment-service

---
+++ src/main/java/ru/corelia/attachments/AttachmentApplication.java
@@ -0,0 +1,27 @@
+package ru.corelia.attachments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import ru.corelia.config.LocalEnvironment;
+
+/** Запускает приложение corelia-attachment-service. */
+@SpringBootApplication(
+        scanBasePackages = {
+            "ru.corelia.config",
+            "ru.corelia.profile",
+            "ru.corelia.support",
+            "ru.corelia.auth",
+            "ru.corelia.http",
+            "ru.corelia.cache",
+            "ru.corelia.integration",
+            "ru.corelia.transport",
+            "ru.corelia.attachments"
+        })
+public class AttachmentApplication {
+    public static void main(String[] args) {
+        var app = new SpringApplication(AttachmentApplication.class);
+        app.setDefaultProperties(LocalEnvironment.load());
+        app.run(args);
+    }
+}
